a95bcd0bceec957880c44ee22ac8f794a9c90310
apudoos <svarg123@gmail.com>
2014-07-23 16:30:34 -0400

    added another class singleFragmentActivity

--- CriminalIntent/src/com/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/src/com/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,11 @@ package com.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
 	@Override
 	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
 		return new CrimeListFragment();
 	}
 
