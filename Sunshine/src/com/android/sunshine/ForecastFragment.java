1c2c54ed9b7ea7b0e71dd43960b3c934952b4e8d
apudoos <svarg123@gmail.com>
2014-08-25 11:42:10 -0400

    added toast message...

--- Sunshine/src/com/android/sunshine/ForecastFragment.java
+++ Sunshine/src/com/android/sunshine/ForecastFragment.java
@@ -27,8 +27,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ForecastFragment extends Fragment {
 	
@@ -85,6 +87,22 @@ public class ForecastFragment extends Fragment {
 		ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
 		
 		listView.setAdapter(mForecastAdapter);
+		
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Log.v("This class", "the id " + id + "position " + position);
+				String forecast = mForecastAdapter.getItem(position);
+				
+				Toast.makeText(getActivity(),  forecast, Toast.LENGTH_LONG).show();
+				
+			}
+		
+		
+		});
 		
 		
 		
