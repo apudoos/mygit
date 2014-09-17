ac667cfaac788359e1ad850ebcd012bbc72904d0
apudoos <svarg123@gmail.com>
2014-09-17 17:03:53 -0400

    initial commit

---
+++ TestSunshine/src/com/android/sunshine/test/FullTestSuite.java
@@ -0,0 +1,21 @@
+package com.android.sunshine.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+public class FullTestSuite extends TestSuite {
+
+	public static Test suite() {
+		return new TestSuiteBuilder(FullTestSuite.class)
+		.includeAllPackagesUnderHere().build();
+	}
+
+	public FullTestSuite() {
+		super();
+	}
+	
+	
+	
+
+}
